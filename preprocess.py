a3d30fbe548b12e28bbd09d3396754e6bc5a14de
spe_ty <62170193+Tangle0307@users.noreply.github.com>
2024-04-28 23:07:00 +0900

    Change processing filter and method

--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,43 @@
 import os
 from PIL import Image
 
-imagedir = os.path.join(os.getcwd(), 'images')
-outputdir = os.path.join(os.getcwd(), 'output')
+image_dir = os.path.join(os.getcwd(), 'images')
+output_dir = os.path.join(os.getcwd(), 'output')
 
 OUT_SIZE = 512
-MIN_RATIO = 0.8
-MAX_RATIO = 1.25
 
 def image_filter(f):
   (width, height) = f.size
-  return (width >= OUT_SIZE) and (height >= OUT_SIZE) and (width >= MIN_RATIO * height) and (width <= MAX_RATIO * height)
+  return (width >= OUT_SIZE) and (height >= OUT_SIZE) 
 
-if not os.path.exists(outputdir):
-  os.mkdir(outputdir)
+def image_process(f):
+  return f.crop(calc_center(f.size)).resize((OUT_SIZE, OUT_SIZE))
 
-for filename in os.listdir(imagedir):
-  with Image.open(os.path.join(imagedir, filename), 'r') as f:
+# return area of processed image
+def calc_center(size):
+  (width, height) = size
+  if (width > height):
+    left = (width - height) / 2
+    right = (width + height) / 2
+    return (left, 0, right, height)
+  else:
+    top = (height - width) / 2
+    bottom = (height + width) / 2
+    return (0, top, width, bottom)
+
+# create output folder
+if not os.path.exists(output_dir):
+  os.mkdir(output_dir)
+
+# process image 
+for filename in os.listdir(image_dir):
+  with Image.open(os.path.join(image_dir, filename), 'r') as f:
     if image_filter(f):
-      f_resized = f.resize((OUT_SIZE, OUT_SIZE))
-      f_resized.save(os.path.join(outputdir, filename), 'JPEG')
+      print( f.width / f.height, f.height / f.width)
+      f_resized = image_process(f)
+      f_resized.save(os.path.join(output_dir, filename), 'JPEG')
 
 # tests
-for filename in os.listdir(outputdir):
-  with Image.open(os.path.join(outputdir, filename), 'r') as f:
+for filename in os.listdir(output_dir):
+  with Image.open(os.path.join(output_dir, filename), 'r') as f:
       assert f.width == OUT_SIZE and f.height == OUT_SIZE
